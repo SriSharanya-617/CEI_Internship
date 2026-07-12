0ceed7b4de1b71ab9d20003f28bd721018aa6d79
SriSharanya-617 <sharanyavinjamuri9917@gmail.com>
2026-07-12 22:30:59 +0530

    Optimize embedding generation

--- Week7_RAG/app.py
+++ Week7_RAG/app.py
@@ -108,8 +108,6 @@ if "GOOGLE_API_KEY" not in st.secrets:
     st.stop()
 
 api_key = st.secrets["GOOGLE_API_KEY"]
-st.write("Key starts with:", api_key[:10])
-st.write("Key length:", len(api_key))
 
 client = genai.Client(
     api_key=api_key
